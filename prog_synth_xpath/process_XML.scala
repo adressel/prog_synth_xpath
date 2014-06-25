20c014e66e14a82bdf59c04a0942ef95b659e3c8
lslsheng <527873752@qq.com>
2014-06-25 13:26:49 -0400

    change for node id

--- prog_synth_xpath/process_XML.scala
+++ prog_synth_xpath/process_XML.scala
@@ -16,9 +16,11 @@ object process_XML{
 	def populate = {
 		for (dtd_element <- DTD.dtd_map){
 			val temp =  DATA.xml \\ dtd_element._1 
+			var node_id = 0
 			for(xml_node <- temp){
 			  total_id += 1
-			  xml_map += (xml_node.toString -> (dtd_element._2, total_id))
+			  node_id += 1
+			  xml_map += (xml_node.toString -> (dtd_element._2, node_id))
 			}
 		}
 //		xml_map.map(s => println(s._1 + " " + s._2))
