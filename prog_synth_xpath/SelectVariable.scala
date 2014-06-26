3623272b4d71b8ef5c9a5cd028d1ff6772e26f49
laian <laian41@gmail.com>
2014-06-26 16:31:06 -0400

    Adds <=, >= and == to SelectVaraible

--- prog_synth_xpath/SelectVariable.scala
+++ prog_synth_xpath/SelectVariable.scala
@@ -5,48 +5,67 @@ import scala.collection.mutable.ArrayBuffer
 abstract class SelectVariable (
   val label: String
 ) extends Variable {
-  
   // matched_nodes keeps track of which nodes are mapped to which
+  def scope = (Data.xml \\ label)
   def matched_nodes : NodeSeq
 }
 
 class AttributeVariable (
   label: String,
-  attr: MetaData
+  attr: MetaData,
+  operator: String
 ) extends SelectVariable(label) {
   def matched_nodes = {
-    (Data.xml \\ label).filter((x => (x \ s"@${attr.key}").toString == attr.value))
+    operator match {
+      case "==" => scope.filter((x => (x \ s"@${attr.key}").toString == attr.value.text))
+      case "<=" => scope.filter((x => (x \ s"@${attr.key}").toString <= attr.value.text))
+      case ">=" => scope.filter((x => (x \ s"@${attr.key}").toString >= attr.value.text))
+    }
   }
-  
-  override def toString = s"$label.${attr.key}=${attr.value}"
+  override def toString = s"$label.${attr.key}${operator}${attr.value}"
 }
 
 class TextVariable (
   label: String,
-  text: String   
+  text: String,
+  operator: String
 ) extends SelectVariable(label) {
   
   def matched_nodes = {
-    (Data.xml \\ label).filter(x => x.text == text)
+    operator match {
+      case "==" => scope.filter((x => x.text == text))
+      case ">=" => scope.filter((x => x.text >= text))
+      case "<=" => scope.filter((x => x.text <= text))
+    }
   }
   
   override def toString = s"<$label> $text"
 }
 
 object SelectVariable {
-  var svs: Vector[SelectVariable] = Vector()
+  var svs: ArrayBuffer[SelectVariable] = ArrayBuffer()
   def all = svs
 
+  def add_attribute_variables(label : String, attr: MetaData) {
+	svs += new AttributeVariable(label, attr, "==")
+	svs += new AttributeVariable(label, attr, "<=")
+	svs += new AttributeVariable(label, attr, ">=")
+  }
+  
+  def add_text_variables(label : String, text : String) {
+    svs += new TextVariable(label, text, "==")
+    svs += new TextVariable(label, text, "<=")
+    svs += new TextVariable(label, text, ">=")
+  }
+  
   def populate = {
     var sv_buffer : ArrayBuffer[SelectVariable] = ArrayBuffer()
     for(node <- Data.xml.descendant) { 
-      for(attr <- node.attributes) {
-        sv_buffer += new AttributeVariable(node.label, attr)
-      }
-      if(node.descendant.length == 1) {
-        sv_buffer += new TextVariable(node.label, node.text)
-      }
+      for(attr <- node.attributes) 
+        add_attribute_variables(node.label, attr)
+        
+      if(node.descendant.length == 1) 
+        add_text_variables(node.label, node.text)
     }
-    svs = sv_buffer.toVector
   }
 }
--- prog_synth_xpath/ian_test.scala
+++ prog_synth_xpath/ian_test.scala
@@ -2,5 +2,9 @@ package prog_synth_xpath
 
 object ian_test extends App {
   SelectVariable.populate
-  SelectVariable.all.foreach(println(_))
+  val jeffery = (Data.xml \\ "Administrator").filter((x => (x \ s"@Extension").toString == "6007"))
+  println(jeffery.length)
+  val attrs = jeffery(0).attributes
+  val ext = attrs.value.text
+  println(ext)
 }
