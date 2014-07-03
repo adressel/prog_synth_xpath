a696e1f4638b545add15d89ed9818d1a5b262b7f
laian <laian41@gmail.com>
2014-07-03 15:32:53 -0400

    comments out main (currently invalid)

--- prog_synth_xpath/main.scala
+++ prog_synth_xpath/main.scala
@@ -1,11 +1,11 @@
 package prog_synth_xpath
 
 object main extends App{
-  NodeVariable.populate
-  user_input.parse_user_nodes
-  user_input.get_userNode_id
-  SelectVariable.populate
-  CNF_maker.create
-  Printer.print_file
-  CNF_solver.solve
+//  NodeVariable.populate
+//  user_input.parse_user_nodes
+//  user_input.get_userNode_id
+//  SelectVariable.populate
+//  CNF_maker.create
+//  Printer.print_file
+//  CNF_solver.solve
 }
